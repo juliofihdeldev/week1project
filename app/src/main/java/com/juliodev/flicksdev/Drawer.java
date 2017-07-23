845a96c003f1166256fa93fdfc6528f9b1412ce0
= <=>
2017-07-23 17:03:48 -0400

    all required and bonus

--- app/src/main/java/com/juliodev/flicksdev/Drawer.java
+++ app/src/main/java/com/juliodev/flicksdev/Drawer.java
@@ -2,9 +2,15 @@ package com.juliodev.flicksdev;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -24,27 +30,35 @@ import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
-public class MovieActivity extends AppCompatActivity {
+public class Drawer extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     private SwipeRefreshLayout swipeContainer;
     ProgressBar progressBar;
-
 
     ArrayList<Movie> movies;
     MovieArrayAdapter movieAdapter;
     ListView lvItems;
 
-    //@TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie);
+        setContentView(R.layout.activity_drawer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         getSupportActionBar().setTitle("HaitiCine");
         // ...
         // Display icon in the toolbar
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setLogo(R.drawable.ic_action_logo);
+        // getSupportActionBar().setLogo(R.drawable.ic_action_logo);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
 
         // Lookup the swipe container view
@@ -69,8 +83,8 @@ public class MovieActivity extends AppCompatActivity {
 
         //load all movies
         loadMovies();
-    }
 
+    }
     public void loadMovies() {
         lvItems = (ListView) findViewById(R.id.lvMovies);
         movies = new ArrayList<>();
@@ -85,7 +99,7 @@ public class MovieActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Movie movie = (Movie) lvItems.getItemAtPosition(position);
                 //Toast.makeText(MoviesActivity.this, movie.getOriginalTitle(), Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(MovieActivity.this, DetailsMovie.class);
+                Intent i = new Intent(Drawer.this, DetailsMovie.class);
                 i.putExtra("movie", movie);
                 startActivity(i);
             }
@@ -137,5 +151,62 @@ public class MovieActivity extends AppCompatActivity {
     public void fetchTimelineAsync(int page) {
         loadRequest();
         swipeContainer.setRefreshing(false);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
--- app/src/main/java/com/juliodev/flicksdev/models/Movie.java
+++ app/src/main/java/com/juliodev/flicksdev/models/Movie.java
@@ -3,12 +3,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  * Created by Julio on 7/12/2016.
  */
-public class Movie {
+public class Movie implements Serializable {
     public String getPosterPath() {
         return  String.format("https://image.tmdb.org/t/p/w342/%s",posterPath);
     }
@@ -28,11 +29,40 @@ public class Movie {
     String orginalTitle;
     String overview;
 
+    public String getVote_count() {
+        return vote_count;
+    }
+
+    String vote_count;
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    String release_date;
+
+
+    public boolean isAdult() {
+        return adult;
+    }
+
+    boolean adult;
+
+    public double getVote_average() {
+        return vote_average;
+    }
+
+    double vote_average;
+
     public Movie (JSONObject jsonObject) throws JSONException{
         this.posterPath = jsonObject.getString("poster_path");
         this.backdropPath = jsonObject.getString("backdrop_path");
         this.orginalTitle = jsonObject.getString("original_title");
         this.overview = jsonObject.getString("overview");
+        this.vote_average = jsonObject.getDouble("vote_average");
+        this.adult = jsonObject.getBoolean("adult");
+        this.release_date = jsonObject.getString("release_date");
+        this.vote_count = jsonObject.getString("vote_count");
      }
     public static ArrayList<Movie> fromJSONArray(JSONArray array)  {
         ArrayList <Movie> results = new ArrayList<>();
